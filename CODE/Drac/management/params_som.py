0000593b3da84428127517721b2ecd6d14efd3af
Hugo Camacho <hocamachoc@gmail.com>
2015-12-16 16:33:01 -0200

    parameters for SOM

--- CODE/Drac/management/params_som.py
+++ CODE/Drac/management/params_som.py
@@ -1,7 +1,10 @@
 from __future__ import print_function
-dict_som=dict([
-	[ 'n_clusters'	, 4		],
-	[ 'tol'		, 1e-4		],
-	[ 'init'	, 'k-means++'	],
-	[ 'n_jobs'	, 1		],
+dict_som = dict([
+    ['nx', 10],
+    ['ny', 10],
+    ['sigma', (10./2.)*(1.-0.1)],
+    ['learn_rate', 0.5],
+    ['random_seed', 200],
+    ['Niter', 100],
+    ['som_type', 'batch']
 ])
