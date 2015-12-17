8467c12a60de25cef77cd951422525d1242b2de5
Naminoshi <michel.aguena@gmail.com>
2015-12-17 20:54:02 -0200

    improve plot_quality.py

--- CODE/Drac/plotting/plot_quality.py
+++ CODE/Drac/plotting/plot_quality.py
@@ -19,26 +19,33 @@ from matplotlib.backends.backend_pdf import PdfPages
 def plot_quality(QUALITY_METHODS,out_name='plots/quality.pdf'):
 	do_pdf=False
 	if out_name[len(out_name)-3:]=='pdf': do_pdf=True
-
 	if do_pdf: pdf	= PdfPages(out_name)
 
 	data		= np.loadtxt('quality_comparison.dat').T
-	xvec		= range(len(data[0]))
-	xint		= [int(x) for x in xvec]
-
 	colors_base	= ['r', 'g', 'b', 'y','c', 'm','k','.8']
 	for n in range(8): colors_base+=colors_base
-	
-	ncls	= set(data[0])
-	ncll	= [int(n) for n in ncls]
-	colors	= [colors_base[ncll.index(dat)] for dat in data[0]]
-
 	xl,yl	= .8,.9
-	hspace	= '10 clusters'
-	for n in range(1,len(ncls)): hspace+='\n10 clusters'
+
+	col_ind	= [ int(n) for n in set(data[0])]
 
 	for Q,ind in zip(QUALITY_METHODS,range(1,1+len(QUALITY_METHODS))):
 		print(ind,Q)
+
+		nan_mask= (data[ind]==data[ind])
+		xdat	= data[0][nan_mask]
+		ydat	= data[ind][nan_mask]
+
+		xvec	= range(len(xdat))
+		xint	= [int(x) for x in xvec]
+		ncls	= set(xdat)
+		ncll	= [int(n) for n in ncls]
+		colors	= [colors_base[col_ind.index(dat)] for dat in xdat]
+		colors_used = [c for c in colors_base if c in colors]
+
+		
+		hspace	= '10 clusters'
+		for n in range(1,len(ncls)): hspace+='\n10 clusters'
+
 		plt.clf()
 		plt.figure(figsize=(16,12))
 		plt.ylabel(Q)
@@ -46,10 +53,10 @@ def plot_quality(QUALITY_METHODS,out_name='plots/quality.pdf'):
 		plt.subplots_adjust(left=0.1, right=0.9, top=0.95, bottom=0.15)
 
 		##################### plot the data  ########################		
-		plt.plot(xvec,data[ind],color='.0',lw=3)
+		plt.plot(xvec,ydat,color='.0',lw=3)
 		if len(ncls)>1:
 			for i in xint:
-				y=data[ind]
+				y=ydat
 				if i==xint[0]	  : xv,yv=[i,i+.5],[ y[i] , y[i]+.5*(y[i+1]-y[i]) ]
 				elif i==xint[-1]  : xv,yv=[i-.5,i],[ y[i]-.5*(y[i]-y[i-1]) , y[i] ]
 				else		  : xv,yv=[i-.5,i,i+.5],[ y[i]-.5*(y[i]-y[i-1]),y[i] , y[i]+.5*(y[i+1]-y[i]) ]
@@ -61,7 +68,7 @@ def plot_quality(QUALITY_METHODS,out_name='plots/quality.pdf'):
 			text.draw(fig.canvas.get_renderer())
 			ex	= text.get_window_extent()
 			t	= matplotlib.transforms.offset_copy(text._transform, y=ex.height/(1.+1./(len(ncls)-1.)), units='dots')
-			for s,c in zip([str(int(n))+' clusters' for n in ncls],[colors_base[n] for n in range(len(ncll))]):
+			for s,c in zip([str(int(n))+' clusters' for n in ncls],colors_used):
 				if s[:2]=='1 ': s=s[:len(s)-1]
 				text = plt.text(xl,yl," "+s+" ",color=c, transform=t)
 			        text.draw(fig.canvas.get_renderer())
