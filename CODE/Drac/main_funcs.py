5a4297c753e52823f5315fd7db08d38564f8ad43
Naminoshi <michel.aguena@gmail.com>
2015-12-17 20:41:36 -0200

    passed CL and RED methods to CONFIG and add update func to CONFIG

--- CODE/Drac/main_funcs.py
+++ CODE/Drac/main_funcs.py
@@ -35,11 +35,11 @@ def PRT_FMSG(msg_ok,msg_fail,FILE):
 #### UPDATE DICTS AND	 ####
 #### CREATE FILE NAMES	 ####
 #############################
-def set_red_dict():
+def set_red_dict(REDUCTION_METHOD):
 	exec('from management.params_red import '+REDUCTION_METHOD+'_dict as dict_red')
 	update_dict(dict_red,REDUCTION_METHOD)
 	return dict_red
-def set_cl_dict():
+def set_cl_dict(CLUSTERING_METHOD):
 	exec('from management.params_clust import '+CLUSTERING_METHOD+'_dict as dict_clust')
 	update_dict(dict_clust,CLUSTERING_METHOD)
 	return dict_clust
@@ -50,15 +50,15 @@ def set_mask():
 	try		: MASK_DATA
 	except NameError: return ''
 	else		: return MASK_DATA
-def set_red(dict_red):
+def set_red(dict_red,REDUCTION_METHOD):
 	try		: REDUCED_DATA_EXTERNAL
 	except NameError: return create_name(REDUCTION_METHOD,dict_red,'red_data/reduced_data_'),REDUCTION_METHOD
 	else		: print('\t- using external reduced data'); return REDUCED_DATA_EXTERNAL,'EXT'
-def set_cl(dict_clust):
+def set_cl(dict_clust,CLUSTERING_METHOD):
 	try		: CLUSTERS_DATA_EXTERNAL
 	except NameError: return create_name(CLUSTERING_METHOD,dict_clust,'cl_data/clustering_'),CLUSTERING_METHOD
 	else		: print('\t- using external clusters');return CLUSTERS_DATA_EXTERNAL,'EXT'
-def set_lab(dict_clust):
+def set_lab(dict_clust,CLUSTERING_METHOD):
 	try		: LABELS_DATA_EXTERNAL
 	except NameError: return create_name(CLUSTERING_METHOD+'_label',dict_clust,'cl_data/clustering_')
 	else		: print('\t- using external labels');return LABELS_DATA_EXTERNAL
@@ -79,14 +79,24 @@ class set_prop:
 		self.PLOTSPEC_INFO	= info_dir + 'plot_spec'	+ info_sufix
 		os.system('mkdir -p '+info_dir)
 
-		self.dict_red	= set_red_dict()
-		self.dict_clust	= set_cl_dict()
+		self.REDUCTION_METHOD	= REDUCTION_METHOD	
+		self.CLUSTERING_METHOD	= CLUSTERING_METHOD	
+
+		self.dict_red	= set_red_dict(self.REDUCTION_METHOD)
+		self.dict_clust	= set_cl_dict(self.CLUSTERING_METHOD)
 
 		self.MASK			= set_mask()
-		self.RED_DATA,self.RED_TYPE	= set_red(self.dict_red)
-		self.CL_DATA,self.CL_TYPE	= set_cl(self.dict_clust)
-		self.LAB_DATA			= set_lab(self.dict_clust)
+		self.RED_DATA,self.RED_TYPE	= set_red(self.dict_red,self.REDUCTION_METHOD)
+		self.CL_DATA,self.CL_TYPE	= set_cl(self.dict_clust,self.CLUSTERING_METHOD)
+		self.LAB_DATA			= set_lab(self.dict_clust,self.CLUSTERING_METHOD)
 		self.SPEC_DATA			= set_spec()
+	def update(self,VAR_TYPE,VAR_PAR,VAL):
+		if VAR_TYPE=='REDUCTION' :
+			if VAR_PAR=='REDUCTION_METHOD'  : self.dict_red, self.REDUCTION_METHOD			= set_red_dict(VAL), VAL
+			else				: self.dict_red[VAR_PAR[len(self.REDUCTION_METHOD)+1:]]	= eval(VAL)
+		if VAR_TYPE=='CLUSTERING': 
+			if VAR_PAR=='CLUSTERING_METHOD' : self.dict_clust, self.CLUSTERING_METHOD			= set_cl_dict(VAL), VAL
+			else				: self.dict_clust[VAR_PAR[len(self.CLUSTERING_METHOD)+1:]]	= eval(VAL)
 		
 
 	
@@ -96,13 +106,13 @@ class set_prop:
 def reducC(CONFIG,case=''):
 	if CONFIG.RED_TYPE =='EXT': print ("REDUCED_DATA_EXTERNAL is defined, please check what you realy want to do!"); exit()
 
-	exec('from reduction.'+REDUCTION_METHOD+' import reduction')
+	exec('from reduction.'+CONFIG.REDUCTION_METHOD+' import reduction')
 	os.system('mkdir -p red_data')
 	try		: ORG_DATA
 	except NameError: ERROR('ORG_DATA key missing!')
 	else		:	
 		np.savetxt(CONFIG.RED_DATA+case,reduction(READ(ORG_DATA),CONFIG.dict_red))
-		print_info(REDUCTION_METHOD,CONFIG.dict_red,'### REDUCTION USED ###',ORG_DATA,CONFIG.REDUCTION_INFO+case)
+		print_info(CONFIG.REDUCTION_METHOD,CONFIG.dict_red,'### REDUCTION USED ###',ORG_DATA,CONFIG.REDUCTION_INFO+case)
 		PRT_FMSG('REDUCTED DATA printed to','REDUCED DATA not printed',CONFIG.RED_DATA+case)
 
 #############################
@@ -111,7 +121,7 @@ def reducC(CONFIG,case=''):
 def clusterC(CONFIG,case_in='',case_out=''):
 	if CONFIG.CL_TYPE =='EXT': print ("CLUSTERS_DATA_EXTERNAL is defined, please check what you realy want to do!"); exit()
 
-	exec('from clustering.'+CLUSTERING_METHOD+' import clustering')
+	exec('from clustering.'+CONFIG.CLUSTERING_METHOD+' import clustering')
 	os.system('mkdir -p cl_data')
 	clusters,labels= clustering(READ(CONFIG.RED_DATA+case_in,CONFIG.MASK),CONFIG.dict_clust)
 	np.savetxt(CONFIG.CL_DATA+case_out,clusters)
@@ -121,7 +131,7 @@ def clusterC(CONFIG,case_in='',case_out=''):
 	else			  : RED_PROP=open(CONFIG.REDUCTION_INFO+case_in,'r').read()
 
 	prt(CONFIG.CLUSTER_INFO+case_out,RED_PROP,'w')
-	print_info(CLUSTERING_METHOD,CONFIG.dict_clust,'### CLUSTERING USED ###',CONFIG.RED_DATA,CONFIG.CLUSTER_INFO+case_out,'a')
+	print_info(CONFIG.CLUSTERING_METHOD,CONFIG.dict_clust,'### CLUSTERING USED ###',CONFIG.RED_DATA,CONFIG.CLUSTER_INFO+case_out,'a')
 	prt(CONFIG.CLUSTER_INFO+case_out,'\n\t-outputs:','a')
 	prt(CONFIG.CLUSTER_INFO+case_out,'n_clusters = '+str(clusters.shape[0]),'a')
 	PRT_FMSG('CLUSTER CENTERS printed to','CLUSTERS CENTERS not printed',CONFIG.CL_DATA+case_out)
@@ -155,14 +165,14 @@ def do_qualityC(CONFIG,case_red='',case=''):
 		for METHOD in QUALITY_METHODS:
 			if METHOD!='':
 				q=check_quality(CONFIG,METHOD,case_red,case)
-				print('\tquality from',METHOD,':',q,'\n')
+				print('\t\tquality from',METHOD,':',q,'\n')
 				prt(CONFIG.QUALITY_INFO+case,'quality from '+METHOD+' = '+str(q),'a')
 				used=True
 				Qvec.append(q)
 		PRT_FMSG('QUALITY results printed to','QUALITY results not printed',CONFIG.QUALITY_INFO+case)
 		if used: return Qvec
 		else:
-			print('\t<no quality checks>')
+			print('\t\t<no quality checks>')
 			prt(CONFIG.QUALITY_INFO+case,'\t<no quality checks>')
 
 #############################
