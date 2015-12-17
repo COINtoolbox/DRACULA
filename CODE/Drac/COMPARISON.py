1ec238224373478b02a894330e1f905ce5a25f74
Naminoshi <michel.aguena@gmail.com>
2015-12-17 20:52:51 -0200

    improve COMPARISON.py to use main_funcs instead of Main

--- CODE/Drac/COMPARISON.py
+++ CODE/Drac/COMPARISON.py
@@ -12,23 +12,14 @@ def PRT_FMSG_PLT(msg_ok,msg_fail,FILE):
 	else:
 		for Q in QUALITY_METHODS: PRT_FMSG(msg_ok,msg_fail,FILE[:-4]+'_'+Q+FILE[-4:])
 
-from config_comparison import *
-import config_comparison
+from config import *
+import config
 
-if os.path.isfile('config.py'):
-	if open('config.py', 'r').readline().split()[0]!='#TEMPCONFIG':
-		print('ATENTION!!!\n\t<< moving config.py to config.safe.py >>\n')
-		os.system('cp config.py config.safe.py')
 
+from main_funcs import *
+CONFIG=set_prop()
 
 def run_comparison():
-	BASE_CONF='#TEMPCONFIG\n'
-	for item in dir(config_comparison):
-		if item[:2]!='__' and item!='VAR_PAR' and item!='VAR_VALS' :
-			exec('temp='+item)
-			if isinstance(temp, str): temp='"'+str(temp)+'"'
-			BASE_CONF += item+'='+str(temp)+'\n'
-	
 	Qmat=[]
 	for VAL,ind in zip(VAR_VALS,range(1,1+len(VAR_VALS))):
 		CASE_LINE="******************* CASE "+str(ind)+" *******************************************"
@@ -38,30 +29,24 @@ def run_comparison():
 		else 			: VAL=str(VAL)
 		case,case_red='.case'+str(ind),''
 	
-		os.system('rm -rf config.pyc')
-		fconf=open('config.py','w')
-		print(BASE_CONF+VAR_PAR+'='+VAL,file=fconf)
-		print(BASE_CONF+VAR_PAR+'='+VAL)
-		fconf.close()
-		import Main
 	
 		print (BORD_LINE);print (CASE_LINE);print (BORD_LINE)
-		reload(Main)
-		from Main import reduc,cluster,do_quality,plot,plot_spec
+		CONFIG.update(VAR_TYPE,VAR_PAR,VAL)
+
 		if VAR_TYPE=='REDUCTION':
 			print ("\t** REDUCTION **\n")
 			case_red=case 
-			reduc(case)
+			reducC(CONFIG,case)
 			print ('\t\t[ok]\n')
 		elif VAR_TYPE=='CLUSTERING': pass 
 		else: print('\t**ERROR** - VAR_TYPE is neither REDUCTION nor CLUSTERING');exit()
 	
 		print ("\t** CLUSTERING **\n")
-		Qvec=[len(set(cluster(case_red,case)[1]))]
+		Qvec=[len(set(clusterC(CONFIG,case_red,case)[1]))]
 		print ('\t\t[ok]\n')
 	
 		print ("\t** QUALITY CHECK **\n")
-		Qmat.append(Qvec+do_quality(case_red,case))
+		Qmat.append(Qvec+do_qualityC(CONFIG,case_red,case))
 		print ('\t\t[ok]\n')
 	
 	fqual=open('quality_comparison.dat','w')
