9a55642b8200d70005e944fc0417686c88294607
Naminoshi <michel.aguena@gmail.com>
2015-12-18 06:58:23 -0200

    fix texts of config_som.py

--- example_configs/config_som.py
+++ example_configs/config_som.py
@@ -1,8 +1,6 @@
 #####################################################################################
 '''
 Original data:
-	Used for the reduction,
-	not needed if doing clusreing only
 '''
 ORG_DATA			= '../../../Dropbox/shared/coin_br/test_data/fake_2clust_2pcacomp.dat'
 
