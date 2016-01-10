059bccc07418d35e7ae0b1a0e775abc8a203f05d
emilleishida <emilleishida@gmail.com>
2016-01-10 11:46:03 +0100

    update default parameters

--- CODE/Drac/management/params_red.py
+++ CODE/Drac/management/params_red.py
@@ -20,7 +20,7 @@ DeepLearning_dict=dict([
 	[ 'training_frame'	, 'train.hex'					],
 	[ 'activation'		, '"Tanh"'					],
 	[ 'autoencoder'		, 'T'						],
-	[ 'hidden'		, 'c(120,100,90,50,30,20,2,20,30,50,90,100,120)'],
+	[ 'hidden'		, 'c(120,100,90,50,30,20,4,20,30,50,90,100,120)'],
 	[ 'epochs'		, '100'						],
 	[ 'ignore_const_cols'	, 'F'						],
 ])
